9f69cd470443fb817adcbfdd62abcc0286ed80f1
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-10-22 22:28:28 +0300

    changes

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+import users from ".//user.json";
+
+export default function App() {
+  return (
+    <div>
+      <Card
+        avatar={users.avatar}
+        username={users.username}
+        tag={users.tag}
+        location={users.location}
+        stats={users.stats}
+      />
+    </div>
+  );
+}
